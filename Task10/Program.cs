14a131d1128ee15a915e4a64153035462d725d0f
spam423 <zombe423@list.ru>
2023-09-28 23:13:49 +0300

    git init

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая: 
+1. принимает на вход трёхзначное число  
+2. на выходе показывает вторую цифру этого числа.
+456 -> 5*/
+
+
+
+Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int FirstNumber(int number)
+{
+    int firstNum = number / 1000;
+    int lastNum = number % 100;
+    return firstNum * 10 + lastNum / 10;
+}
+
+int res = FirstNumber(num);
+
+Console.WriteLine(res);
---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.*/
+
+
+Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99)
+{
+    string str = num.ToString();
+    Console.WriteLine(str[2]);
+}
+if (num<99)
+{
+    Console.WriteLine("no third");
+}
